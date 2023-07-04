58fb3b10781ad5d35879d7c24262ec6b77aa1d36
katungi <dankatdennis@gmail.com>
2023-07-04 10:41:41 +0300

    change metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import { useSpeechRecognition } from "react-speech-recognition"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <div className='w-full'>
+      <div className='w-3/4 max-w-screen-md pt-4 m-auto'>
+        <div className='pt-12 font-extrabold text-8xl pb-8'>
+          <h2>Not Cortana</h2>
+        </div>
+      </div>
+    </div>
   )
 }
